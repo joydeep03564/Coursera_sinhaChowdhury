737938e381290ab53490324474afc48fe40b45c5
joydeep sinha chowdhury <joydeep03564@gmail.com>
2018-07-25 17:16:40 +0530

    Creating intial coursera Repositort

---
+++ README.md
@@ -0,0 +1,20 @@
+###******************************************************************************
+# Copyright (C) 2018 by Joydeep Sinha Chowdhury
+#
+# Redistribution, modification or use of this software in source or binary
+# forms is permitted as long as the files maintain this copyright. Users are 
+# permitted to modify this and use it to learn about the field of embedded
+# software. Joydeep Sinha Chowdhury and the University of Colorado are not liable for any
+# misuse of this material. 
+#
+###*****************************************************************************
+
+
+### This is a free access repository for the Coursera Specialization of Embedded
+### Software Essentials to use in conjunction with the course1, the Introductio
+### to Embedded Software and Development Environments.
+
+### The repository is organized in multiple folders:
+###      stats.c : This file is consisting of a program that calaculates the statistics of a given data. This calculates, mean,meandian, min ###      and max value of given data set.        
+###      stats.h: This contains the header file that are used in stats.c   
+### @Joydeep Sinha Chowdhury for embedded software development course
---
+++ stats.h
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * Copyright (C) 2017 by joydeep sinha chowdhury - for the course University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. joydeep sinha chowdhury and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <stats.h> 
+ * @brief<@ this header file contains :
+* print_Array function: Prints the given array.
+* Find_maximum : This function is also taking two aurguments and calculating  the maximum from the given i/p array by comapring each element.
+* Find_minimum : This function is also taking two aurguments and calculating  the minimum from the given i/p array by comapring each element.
+* Find_mean: This function reads through the array and sum it up and then calculates the mean based on the number of the element and value of *the element entered by the User.
+* sort_array: This function is passing the array element and number of the element. Then performs the shorting by comparing each element and * * gives the output of the shorted array in decending sequence
+* find_median: This function is passing the array element and number of the element. Then based on the number of elements. It will calculate *the median from the sorted array. If the number of the elements is odd then it will give the middle element value as a median and if the *number of elements is even then it will give the averge of (n-1/2 th element and n/2 )/2 as a Median. 
+* Print_statistics(): This function is only print the values from functions: Find_maximum,Find_minimum,Find_mean,find_median;
+ *
+ * 
+ *
+ * @author <Joydeep Sinha Chowdhury>
+ * @date <25-07-2018 >
+ *
+ **/
+#ifndef __STATS_H__
+#define __STATS_H__
+
+
+#endif /* __STATS_H__ */
